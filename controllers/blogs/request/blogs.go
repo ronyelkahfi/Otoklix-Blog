1c0e9b5ae16afde3b5c405b2169ca634cc8a8a6a
Fatkhur Rony <ronyelkahfi@gmail.com>
2022-02-05 13:20:46 +0700

    fix bug : correct request parameter

--- controllers/blogs/request/blogs.go
+++ controllers/blogs/request/blogs.go
@@ -1,8 +1,20 @@
 package request
 
+import (
+	_blogDomain "otoklix-blog/business/blogs"
+	"time"
+)
+
 type BlogRequest struct {
-	Id           uint   `json:"id"`
-	Title        string `json:"title"`
-	Content      string
-	Published_at string
+	Title        string    `json:"title"`
+	Content      string    `json:"content"`
+	Published_at time.Time `json:"published_at"`
+}
+
+func RequestToDomain(data BlogRequest) _blogDomain.Domain {
+	return _blogDomain.Domain{
+		Title:        data.Title,
+		Content:      data.Content,
+		Published_at: data.Published_at,
+	}
 }
