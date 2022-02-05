079fe51958fecd6f6daaf48601f5ed45df18a9c5
Fatkhur Rony <ronyelkahfi@gmail.com>
2022-02-05 13:12:52 +0700

    core : create main file

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"log"
+	_blogUsecase "otoklix-blog/business/blogs"
+	_blogController "otoklix-blog/controllers/blogs"
+	_dbDriver "otoklix-blog/drivers/databases"
+	_blogRepo "otoklix-blog/drivers/databases/blogs"
+	"time"
 
+	_route "otoklix-blog/apps/routes"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("apps/configs/config.json")
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+	configDB := _dbDriver.ConfigDB{
+		DB_Username: viper.GetString("database.user"),
+		DB_Password: viper.GetString("database.pass"),
+		DB_Host:     viper.GetString("database.host"),
+		DB_Port:     viper.GetString("database.port"),
+		DB_Database: viper.GetString("database.name"),
+	}
+
+	db := configDB.ConnectDB()
+	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+
+	blogRepo := _blogRepo.NewBlogRepository(db)
+	blogUseCase := _blogUsecase.NewUseCase(blogRepo, timeoutContext)
+	blogController := _blogController.NewController(blogUseCase)
+
+	e := echo.New()
+	routesInit := _route.ControllerList{
+		BlogController: *blogController,
+	}
+	routesInit.RouteRegister(e)
+
+	log.Fatal(e.Start(viper.GetString("server.address")))
 }
