7eba8f0f69e8852efb036cb28f5f77538eb506a8
Fatkhur Rony <ronyelkahfi@gmail.com>
2022-02-05 13:19:37 +0700

    Perf : simplify the function name

--- controllers/blogs/http.go
+++ controllers/blogs/http.go
@@ -3,7 +3,9 @@ package blogs
 import (
 	_blogDomain "otoklix-blog/business/blogs"
 	_controllers "otoklix-blog/controllers"
+	_request "otoklix-blog/controllers/blogs/request"
 	_response "otoklix-blog/controllers/blogs/response"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,7 +14,7 @@ type BlogController struct {
 	usecase _blogDomain.Usecase
 }
 
-func NewBlogController(BlogUsecase _blogDomain.Usecase) *BlogController {
+func NewController(BlogUsecase _blogDomain.Usecase) *BlogController {
 	return &BlogController{
 		usecase: BlogUsecase,
 	}
@@ -24,4 +26,55 @@ func (bc *BlogController) GetPost(c echo.Context) error {
 		return _controllers.NewErrorResponse(c, err)
 	}
 	return _controllers.NewSuccessResponse(c, _response.ListDomainToResponse(data))
+}
+
+func (bc *BlogController) ShowPost(c echo.Context) error {
+	ctx := c.Request().Context()
+	id, _ := strconv.Atoi(c.Param("id"))
+	data, err := bc.usecase.ShowPost(ctx, uint(id))
+	if err != nil {
+		return _controllers.NewErrorResponse(c, err)
+	}
+	return _controllers.NewSuccessResponse(c, _response.DomainToResponse(data))
+}
+
+func (bc *BlogController) CreatePost(c echo.Context) error {
+	ctx := c.Request().Context()
+	var data _request.BlogRequest
+	if err := c.Bind(&data); err != nil {
+		return _controllers.NewErrorResponse(c, err)
+	}
+	dataDomain := _request.RequestToDomain(data)
+	dataResponse, err := bc.usecase.CreatePost(ctx, dataDomain)
+	if err != nil {
+		return _controllers.NewErrorResponse(c, err)
+	}
+	return _controllers.NewSuccessResponse(c, _response.DomainToResponse(dataResponse))
+}
+
+func (bc *BlogController) UpdatePost(c echo.Context) error {
+	ctx := c.Request().Context()
+	id, _ := strconv.Atoi(c.Param("id"))
+	var data _request.BlogRequest
+	if err := c.Bind(&data); err != nil {
+		return _controllers.NewErrorResponse(c, err)
+	}
+	dataDomain := _request.RequestToDomain(data)
+	dataDomain.Id = uint(id)
+
+	dataResponse, err := bc.usecase.UpdatePost(ctx, dataDomain)
+	if err != nil {
+		return _controllers.NewErrorResponse(c, err)
+	}
+	return _controllers.NewSuccessResponse(c, _response.DomainToResponse(dataResponse))
+}
+func (bc *BlogController) DeletePost(c echo.Context) error {
+	ctx := c.Request().Context()
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	dataResponse, err := bc.usecase.DeletePost(ctx, uint(id))
+	if err != nil {
+		return _controllers.NewErrorResponse(c, err)
+	}
+	return _controllers.NewSuccessResponse(c, _response.DomainToResponse(dataResponse))
 }
